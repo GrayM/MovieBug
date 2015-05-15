ba6a075f15a408cb6d14e401c7b602183a0976c4
Michael Gray <michael.jgray01@gmail.com>
2015-05-15 15:05:48 -0500

    trimmed

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
             $('#results').empty();
         }
     });
-    var getPoster = function() {
+    var showPoster = function() {
         var film = $('#movieNameInput').val();
         if (film == '') {
             $('#results').html("<h3 class='loading'>Bzzzt... are you sure you typed something? Bzzt!</h3>");
@@ -13,26 +13,37 @@ $(document).ready(function() {
             $('#results').html("<h3 class='loading'>Searching database for " + film + "..." + "</h3>");
         }
     }
+
     function successCallback(data) {
+        'use strict';
+        $('#results').text('');
+        data = JSON.parse(data);
+        console.log(data);
+        if (data.results && data.results.length > 0) {
+            var posterURL = theMovieDb.common.images_uri + 'w500' + data.results[0].poster_path;
+            $('#results').append('<h4>Here you go, insect!</h4></br />');
+            $('#results').append('Title: <b>' + data.results[0].title + '</b><br />');
+            $('#results').append('Release date: <b>' + data.results[0].release_date + '</b><br />');
+            $('#results').append('Movie Id: <b>' + data.results[0].id + '</b><br />');
+            $('#results').append('<img src="' + posterURL + '" />');
+        } else {
+            $('#results').append('<h4>I couldn\'t find nuthin... quit buggin\' me!</h4>');
+            console.log('No results');
+        }
+    }
+
+    function successActor(data) {
             'use strict';
             $('#results').text('');
             data = JSON.parse(data);
-            if (data.results && data.results.length > 0) {
-                var baseURL = theMovieDb.common.base_uri;
-                var posterURL = theMovieDb.common.images_uri + 'w300' + data.results[0].poster_path;
-                $.get(baseURL + 'movie/top_rated', function(data) {
-                    $('#results').html(data);
-                    alert('load');
-                });
-                $('#results').append('<h4>Here you go, insect!</h4></br />')
-                $('#results').append('Title: <b>' + data.results[0].title + '</b><br />');
-                $('#results').append('Release date: <b>' + data.results[0].release_date + '</b><br />');
-                $('#results').append('Movie Id: <b>' + data.results[0].id + '</b><br />');
-                $('#results').append('<img src="' + posterURL + '" />');
-            } else {
-                $('#results').append('<h4>I couldn\'t find nuthin... quit buggin\' me!</h4>');
-                console.log('No results');
-            }
+            console.log(data);
+            var actorID = data.results[0].id;
+            var actorInfo = theMovieDb.common.base_uri + 'person/' + actorID + '/movie_credits';
+            var profile = theMovieDb.common.base_uri + 'person/' + actorID + data.results[0].profile_path;
+            $('#results').append('<h4>Here you go, insect!</h4></br />');
+            $('#results').append('Best known for: <b>' + data.results[0].known_for[0].title + '</b><br />');
+            $('#results').append('<img src="' + theMovieDb.common.images_uri + 'w500' + data.results[0].known_for[0].poster_path + '" />' + '</b><br />');
+
         }
         // callback for movie search error
     function errorCallback(data) {
@@ -46,7 +57,7 @@ $(document).ready(function() {
         // search button click event handler
     $('#searchBtn').click(function() {
         'use strict';
-        getPoster();
+        showPoster();
         var searchTerm = $('#movieNameInput').val(),
             searchYear = $('#movieYearInput').val(),
             options = {
@@ -57,23 +68,31 @@ $(document).ready(function() {
             options.year = searchYear;
         }
         theMovieDb.search.getMovie(options, successCallback, errorCallback);
+
     });
+
     // input enter/return keyup event handler
-    $('#movieNameInput').keyup(function(event) {
+    $('#movieNameInput, #actorInput').keyup(function(event) {
         if (event.keyCode === 13) {
             'use strict';
-            getPoster();
+            showPoster();
             var searchTerm = $('#movieNameInput').val(),
                 searchYear = $('#movieYearInput').val(),
+                searchActor = $('#actorInput').val()
                 options = {
-                    "query": searchTerm
+                    "query": searchTerm,
                 };
-            // options.query = searchTerm;
-            if (!isNaN(searchYear)) {
-                options.year = searchYear;
-            }
+            options.query = searchTerm;
+
+            options1 = {
+                    "query": searchActor
+                };
+            options1.query = searchActor;
             theMovieDb.search.getMovie(options, successCallback, errorCallback);
+            theMovieDb.search.getPerson(options1, successActor, errorCallback);
+
         }
     });
+
 
 });
